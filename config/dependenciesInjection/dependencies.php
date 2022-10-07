31cb3e0bff49750a13b7f46a95a4cf14c4c66c3a
mohamed <adeboss_3@hotmail.fr>
2022-10-07 15:36:16 +0200

    mise en route du routeur

--- config/dependenciesInjection/dependencies.php
+++ config/dependenciesInjection/dependencies.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Controller\CountryController;
+use App\Z\Routing\Router;
+use App\Z\Routing\RouterInterface;
 use Symfony\Component\HttpFoundation\Request;
 
-return [
-    Request:: class=> Request :: createFromGlobals()
+    return [
 
-];
+        Request::class => Request::createFromGlobals(),
+
+        'controllers' => [
+            "CountryController" =>CountryController::class
+        ],
+
+        RouterInterface::class => DI\create(Router::class)
+        ->constructor(DI\get(Request::class), DI\get('controllers'))
+        ,
+
+    ];
---
+++ src/Controller/CountryController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller;
+
+    class CountryController
+    {
+
+        #[Route('/', name: 'country.index', methods: ['GET'])]
+        public function index()
+        {
+            dd('page index');
+        }
+
+        // #[Route('/create', name: 'country.create', methods: ['GET'])]
+        // public function create()
+        // {
+
+        // }
+    }
